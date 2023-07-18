df6efe0b39d297b26cd351a53c3be8b10f173fe2
Roylouis-web <powellleroy488@gmail.com>
2023-07-18 09:11:04 -0700

    All done

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,31 +1,33 @@
 #!/usr/bin/env python3
 
 '''
-    Module for a python script that provides some
-    stats about Nginx logs stored in MongoDB
+    A script that prints some stats about
+    an nginx collection
 '''
 
 from pymongo import MongoClient
 
-client = MongoClient('mongodb://127.0.0.1:27017')
-nginx_collection = client.logs.nginx
-methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
-total = nginx_collection.count_documents({})
-get_and_path = nginx_collection.count_documents(
+
+if __name__ == '__main__':
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = client.logs.nginx
+    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    total = nginx_collection.count_documents({})
+    get_and_path = nginx_collection.count_documents(
         {'method': 'GET', 'path': '/status'})
-method_dict = {}
+    method_dict = {}
 
-for method in methods:
-    method_dict.update(
+    for method in methods:
+        method_dict.update(
             {method: nginx_collection.count_documents({'method': method})})
-print('{} logs'.format(total))
-print('Methods:')
-for method in methods:
-    print('\tmethod {}: {}'.format(method, method_dict[method]))
+    print('{} logs'.format(total))
+    print('Methods:')
+    for method in methods:
+        print('\tmethod {}: {}'.format(method, method_dict[method]))
 
-print('{} status check'.format(get_and_path))
+    print('{} status check'.format(get_and_path))
 
-print('IPs:')
-for res in nginx_collection.aggregate(
-        [{'$sortByCount': '$ip'}, {'$limit': 10}]):
-    print("\t{}: {}".format(res['_id'], res['count']))
+    print('IPs:')
+    for res in nginx_collection.aggregate(
+            [{'$sortByCount': '$ip'}, {'$limit': 10}]):
+        print("\t{}: {}".format(res['_id'], res['count']))
