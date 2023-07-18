ec8c217a642d4ff8463adea54241eae145a4d984
Roylouis-web <powellleroy488@gmail.com>
2023-07-18 09:15:45 -0700

    All done

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+'''
+    A script that prints some stats about
+    an nginx collection
+'''
+
+from pymongo import MongoClient
+
+
+if __name__ == '__main__':
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = client.logs.nginx
+    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    total = nginx_collection.count_documents({})
+    get_and_path = nginx_collection.count_documents(
+        {'method': 'GET', 'path': '/status'})
+    method_dict = {}
+
+    for method in methods:
+        method_dict.update(
+            {method: nginx_collection.count_documents({'method': method})})
+    print('{} logs'.format(total))
+    print('Methods:')
+    for method in methods:
+        print('\tmethod {}: {}'.format(method, method_dict[method]))
+
+    print('{} status check'.format(get_and_path))
