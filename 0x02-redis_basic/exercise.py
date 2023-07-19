6e4d429faede9b3fde41381a2794cbefc6086cb2
Roylouis-web <powellleroy488@gmail.com>
2023-07-19 17:16:49 +0000

    All done

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -6,7 +6,7 @@
 
 import redis
 from uuid import uuid4
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache(object):
@@ -33,3 +33,33 @@ class Cache(object):
         random_key = str(uuid4())
         self._redis.set(random_key, data)
         return random_key
+
+    def get(key: str, fn=None: Optional[Callable]) ->
+    Union[bytes, str, int, float]:
+
+        """
+            A method that takes a key string as
+            argument and an optional Callable
+        """
+
+        value = self._redis.get(key)
+
+        if fn:
+            value = fn(value)
+        return value
+
+    def get_str(key: str) -> str:
+        """
+            A method that automatically
+            parametrizes Cache.get
+        """
+
+        return self._redis(key).decode('utf-8')
+
+    def get_int(key: str) -> int:
+        """
+            A method that automaticallu
+            parametrizes Cache.get
+        """
+
+        return int(self._redis.get(key))
