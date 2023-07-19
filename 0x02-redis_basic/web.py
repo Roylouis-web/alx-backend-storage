5303a82adc798d3d4e29a39f9da1c314971f52bf
Roylouis-web <powellleroy488@gmail.com>
2023-07-19 23:29:39 +0000

    All done

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -22,18 +22,11 @@ def count_url_access(method: Callable[[str], str]) -> Callable:
     def wrapper(url: str) -> str:
         ''' Wrapper decorator '''
 
-        cached_key = 'cached:' + url
-        cached_data = store.get(cached_key)
-
-        if cached_data:
-            return cached_data.decode('utf-8')
-
         count_key = 'count:' + url
         html = method(url)
 
         store.incr(count_key)
-        store.set(cached_key, html)
-        store.expire(cached_key, 10)
+        store.expire(count_key, 10)
         return html.text
     return wrapper
 
