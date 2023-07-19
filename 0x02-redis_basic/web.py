aae9148069ca3c1ebd0b61e78021f7f8f85247a5
Roylouis-web <powellleroy488@gmail.com>
2023-07-19 23:37:21 +0000

    All done

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -6,35 +6,15 @@
 
 import requests
 import redis
-from functools import wraps
-from typing import Callable
 
 store = redis.Redis()
 
 
-def count_url_access(method: Callable[[str], str]) -> Callable:
-    '''
-        Decorator to count the number of times
-        an https request was made to a url
-    '''
-
-    @wraps(method)
-    def wrapper(url: str) -> str:
-        ''' Wrapper decorator '''
-
-        count_key = 'count:' + url
-        html = method(url)
-
-        store.incr(count_key)
-        store.expire(count_key, 10)
-        return html.text
-    return wrapper
-
-
-@count_url_access
 def get_page(url: str) -> str:
     '''
         Returns HTML content from a url
     '''
     res = requests.get(url)
+    store.incr('count:{}'.format(url))
+    store.expire('count:{}'.format(url), 10)
     return res.text
