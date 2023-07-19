c85fff1770c97ede66c36c34f0dc078704355a3a
Roylouis-web <powellleroy488@gmail.com>
2023-07-19 17:35:51 +0000

    All done

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -34,7 +34,7 @@ class Cache(object):
         self._redis.set(random_key, data)
         return random_key
 
-    def get(key: str, fn=None: Optional[Callable]) ->
+    def get(key: str, fn: Optional[Callable] = None) ->
     Union[bytes, str, int, float]:
 
         """
